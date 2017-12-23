97098a1f94237d1c0bcf78d97271a93f7a549b6b
Enda Xue <xueenda@gmail.com>
2017-12-22 21:57:01 -0500

    Update Remove Duplicates from Sorted Array.java

--- Easy/Remove Duplicates from Sorted Array.java
+++ Easy/Remove Duplicates from Sorted Array.java
@@ -8,7 +8,8 @@ Do not allocate extra space for another array, you must do this in place with co
 For example,
 Given input array nums = [1,1,2],
 
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
+It doesn't matter what you leave beyond the new length.
 */
 
 
